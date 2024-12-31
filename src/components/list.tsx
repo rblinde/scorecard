2cb41efe2a92b17796932d421fa603cf4e38e40f
rblinde <16778947+rblinde@users.noreply.github.com>
2024-12-31 16:19:08 +0100

    feat: add default sorting function

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -9,21 +9,23 @@ export function List() {
 
   return (
     <div className="flex flex-col gap-2 mt-4">
-      {store.map((player, i) => (
-        <div
-          key={player.id}
-          className="p-3 flex items-center justify-between border rounded-md shadow"
-        >
-          <div className="flex gap-2 items-center flex-1">
-            <span className="text-blue-600">#{i + 1}</span>
-            <PlayerName player={player} />
+      {store
+        .sort((a, b) => b.score - a.score)
+        .map((player, i) => (
+          <div
+            key={player.id}
+            className="p-3 flex items-center justify-between border rounded-md shadow"
+          >
+            <div className="flex gap-2 items-center flex-1">
+              <span className="text-blue-600">#{i + 1}</span>
+              <PlayerName player={player} />
+            </div>
+            <div className="flex gap-4 items-center">
+              <span className="font-semibold text-blue-900">{player.score}</span>
+              <ScoreButtons id={player.id} />
+            </div>
           </div>
-          <div className="flex gap-4 items-center">
-            <span className="font-semibold text-blue-900">{player.score}</span>
-            <ScoreButtons id={player.id} />
-          </div>
-        </div>
-      ))}
+        ))}
 
       {!store.length && (
         <p className="pt-4 text-sm text-center text-muted-foreground">
